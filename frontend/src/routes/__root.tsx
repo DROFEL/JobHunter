f0cd769dd687edfb8a0685e91cd2e1cd2d2310b8
George <cradplay@gmail.com>
2026-04-19 16:14:09 -0400

    frontend init

---
+++ frontend/src/routes/__root.tsx
@@ -0,0 +1,11 @@
+import { Outlet, createRootRoute } from "@tanstack/react-router"
+
+export const Route = createRootRoute({
+  component: Root,
+})
+
+function Root() {
+  return (
+    <Outlet />
+  )
+}
